de76394345a27029b09ad091defcbf33ff99bc1a
nathayoung <nathanayoung@msn.com>
2021-11-26 17:18:37 -0500

    Completing the Building a Personal Budget Report with Python project

---
+++ Projects/Python/Build A Personal Budget Report with Python Collections and Iterables/budget/FrequentExpenses.py
@@ -0,0 +1,40 @@
+## Count Expressions by Category
+
+# Step 1
+from . import Expense
+# Step 4
+import collections
+# Step 7
+import matplotlib.pyplot as plt
+
+# Step 2
+expenses = Expense.Expenses()
+
+expenses.read_expenses("data/spending_data.csv")
+
+# Step 3
+spending_categories = []
+
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
+# Step 4
+spending_counter = collections.Counter(spending_categories)
+
+#print(spending_counter)
+
+# Step 5
+top5 = spending_counter.most_common(5)
+
+# Step 6
+categories, count = zip(*top5)
+
+# Step 7
+fig, ax = plt.subplots()
+
+# Step 8
+ax.bar(categories, count)
+ax.set_title("# of Purchases by Category")
+
+# Step 9
+plt.show()
