2b4ca3bfdf4f2ac40020a1f0d9650b707144c084
nathayoung <nathanayoung@msn.com>
2022-09-26 22:01:52 -0400

    finishing python project

--- Projects/Python/Decoding Sensor Data Using Python/sensor/temperature_info.py
+++ Projects/Python/Decoding Sensor Data Using Python/sensor/temperature_info.py
@@ -5,8 +5,10 @@ from house_info import HouseInfo
 class TemperatureData(HouseInfo):
     def _convert_data(self, data):
         recs = []
+
         for rec in data:
             recs.append(int(rec, base=10))
+
         return recs
 
     def get_data_by_area(self, rec_area=0):
